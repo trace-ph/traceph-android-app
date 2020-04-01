d5d62a9099764275a65819bb27f18cf190ec1ad2
gabrielslach <gabdrix09@gmail.com>
2020-04-01 17:59:41 +0800

    + GATT broadcast

--- android/app/src/main/java/com/traceph/BleModule.java
+++ android/app/src/main/java/com/traceph/BleModule.java
@@ -6,12 +6,23 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothProfile;
 import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.BluetoothManager;
 import android.os.ParcelUuid;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseCallback;
 import android.util.Log;
+import android.content.Context;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
 
 import java.nio.charset.Charset;
 import java.util.UUID;
@@ -30,6 +41,80 @@ public class BleModule extends ReactContextBaseJavaModule {
         return "BleModule";
     }
 
+
+    private BluetoothLeAdvertiser advertiser;
+    private BluetoothManager mBluetoothManager;
+    private BluetoothGattServer mBluetoothGattServer;
+
+    private static UUID myUUID = UUID.fromString("CDB7920D-72F1-4D4D-8E47-C090302DBD13");
+
+
+    @ReactMethod
+    private void advertise(Callback advCallBack) {
+        BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
+        advertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+        if (advertiser == null) {
+            Toast.makeText(getReactApplicationContext(), "Failed to create advertiser", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                .setConnectable(true)
+                .build();
+
+        ParcelUuid pUuid = new ParcelUuid(myUUID);
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName( false )
+                .addServiceUuid(pUuid)
+                .setIncludeTxPowerLevel(false)
+                .build();
+
+        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                advCallBack.invoke(true);
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                advCallBack.invoke(false, errorCode);
+                Log.e("BLE", "Advertising onStartFailure: " + errorCode);
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        advertiser.startAdvertising(settings, data, advertisingCallback);
+    }
+
+    @ReactMethod
+    private void startServer(Callback srvCallBack) {
+        mBluetoothGattServer = mBluetoothManager.openGattServer(getReactApplicationContext(), mGattServerCallback);
+        if (mBluetoothGattServer == null) {
+            srvCallBack.invoke(false);
+            return;
+        }
+
+        mBluetoothGattServer.addService(new BluetoothGattService(myUUID,
+                BluetoothGattService.SERVICE_TYPE_PRIMARY));
+
+        srvCallBack.invoke(true);
+    }
+
+    private BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                return;
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                return;
+            }
+        }
+    };
+
     // Custom function that we are going to export to JS
     @ReactMethod
     public void isAdvertisingSupported(
@@ -41,60 +126,46 @@ public class BleModule extends ReactContextBaseJavaModule {
             } catch (Exception e) {
                 Toast.makeText(getReactApplicationContext(), "Multiple advertisement not supported(1)", Toast.LENGTH_SHORT).show();
             }
-        } else successCallback.invoke(true);
+        } else {
+            successCallback.invoke(true);
+            mBluetoothManager = (BluetoothManager) getReactApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
+        }
     }
 
-    private BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+    private boolean adv = false;
+    private boolean srv = false;
+    private int err;
 
     @ReactMethod
-    public void advertise(Callback successCallBack) {
-        //BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
-
-        AdvertiseSettings settings = new AdvertiseSettings.Builder()
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
-                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH).setConnectable(false).build();
-
-        ParcelUuid pUuid = new ParcelUuid(UUID.fromString("CDB7920D-72F1-4D4D-8E47-C090302DBD13"));
-
-        AdvertiseData data = new AdvertiseData.Builder().setIncludeDeviceName( false ).addServiceUuid(pUuid).setIncludeTxPowerLevel(false).build();
+    public void stopBroadcastingGATT(Callback advsrvCallBack) {
 
         AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
             @Override
             public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                successCallBack.invoke(true);
+                adv = true;
                 super.onStartSuccess(settingsInEffect);
             }
 
             @Override
             public void onStartFailure(int errorCode) {
-                successCallBack.invoke(false, errorCode);
-                Log.e("BLE", "Advertising onStartFailure: " + errorCode);
+                adv = false;
+                err = errorCode;
                 super.onStartFailure(errorCode);
             }
         };
+        if (advertiser == null) {
+            adv = true;
+        } else {
+            advertiser.stopAdvertising(advertisingCallback);
+            adv = true;
+        }
 
-        advertiser.startAdvertising(settings, data, advertisingCallback);
-    }
-
-    @ReactMethod
-    public void stopAdvertise(Callback successCallBack) {
-
-        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
-            @Override
-            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
-                successCallBack.invoke(true);
-                super.onStartSuccess(settingsInEffect);
-            }
-
-            @Override
-            public void onStartFailure(int errorCode) {
-                successCallBack.invoke(false, errorCode);
-                Log.e("BLE", "Advertising onStartFailure: " + errorCode);
-                super.onStartFailure(errorCode);
-            }
-        };
-
-        advertiser.stopAdvertising(advertisingCallback);
-        successCallBack.invoke(true);
+        if (mBluetoothGattServer == null) {
+            srv = true;
+        } else {
+            mBluetoothGattServer.close();
+            srv = true;
+        }
+        advsrvCallBack.invoke(adv,srv, err);
     }
 }
